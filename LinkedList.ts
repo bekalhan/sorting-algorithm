e85c08dbe50c897fc5cf3f1f173f0fc289acd9d4
root07 <77151270+bekalhan@users.noreply.github.com>
2022-12-13 07:44:32 -0800

    linked list updated

--- LinkedList.ts
+++ LinkedList.ts
@@ -1,90 +1,90 @@
+import { Sorter } from './Sorter';
+
 class Node {
-    next: Node | null = null;
-  
-    constructor(public data: number) {}
+  next: Node | null = null;
+
+  constructor(public data: number) {}
+}
+
+export class LinkedList extends Sorter {
+  head: Node | null = null;
+
+  add(data: number): void {
+    const node = new Node(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
   }
-  
-  export class LinkedList {
-    head: Node | null = null;
-  
-    add(data: number): void {
-      const node = new Node(data);
-  
-      if (!this.head) {
-        this.head = node;
-        return;
-      }
-  
-      let tail = this.head;
-      while (tail.next) {
-        tail = tail.next;
-      }
-  
-      tail.next = node;
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
     }
-  
-    get length(): number {
-      if (!this.head) {
-        return 0;
-      }
-  
-      let length = 1;
-      let node = this.head;
-      while (node.next) {
-        length++;
-        node = node.next;
-      }
-  
-      return length;
+
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
     }
-  
-    at(index: number): Node {
-      if (!this.head) {
-        throw new Error('Index out of bounds');
-      }
-  
-      let counter = 0;
-      let node: Node | null = this.head;
-      while (node) {
-        if (counter === index) {
-          return node;
-        }
-  
-        counter++;
-        node = node.next;
-      }
-  
+
+    return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head) {
       throw new Error('Index out of bounds');
     }
-  
-    compare(leftIndex: number, rightIndex: number): boolean {
-      if (!this.head) {
-        throw new Error('List is empty');
+
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
       }
-  
-      return this.at(leftIndex).data > this.at(rightIndex).data;
+
+      counter++;
+      node = node.next;
     }
-  
-    swap(leftIndex: number, rightIndex: number): void {
-      const leftNode = this.at(leftIndex);
-      const rightNode = this.at(rightIndex);
-  
-      const leftHand = leftNode.data;
-      leftNode.data = rightNode.data;
-      rightNode.data = leftHand;
+
+    throw new Error('Index out of bounds');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error('List is empty');
     }
-  
-    print(): void {
-      if (!this.head) {
-        return;
-      }
-  
-      let node: Node | null = this.head;
-      while (node) {
-        console.log(node.data);
-        node = node.next;
-      }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
     }
   }
-  
-  
+}
