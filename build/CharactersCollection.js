1e9a750956e6120871792696a0004972f7dfb7b1
root07 <77151270+bekalhan@users.noreply.github.com>
2022-12-13 07:41:52 -0800

    linked list

---
+++ build/CharactersCollection.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CharactersCollection = void 0;
+class CharactersCollection {
+    constructor(data) {
+        this.data = data;
+    }
+    get length() {
+        return this.data.length;
+    }
+    compare(leftIndex, rightIndex) {
+        return (this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase());
+    }
+    swap(leftIndex, rightIndex) {
+        const characters = this.data.split('');
+        const leftHand = characters[leftIndex];
+        characters[leftIndex] = characters[rightIndex];
+        characters[rightIndex] = leftHand;
+        this.data = characters.join('');
+    }
+}
+exports.CharactersCollection = CharactersCollection;
--- build/index.js
+++ build/index.js
@@ -1,15 +1,12 @@
-import { Sorter } from './Sorter';
-import { NumberCollection } from './NumbersCollection';
-import { CharactersCollection } from './CharactersCollection';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
-
 /*
 const charactersCollection = new CharactersCollection('Xaayb');
 const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(charactersCollection.data);
-*/
+*/ 
